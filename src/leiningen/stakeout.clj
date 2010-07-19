5da425b6853f3a522cda29edf98153af7158bec0
John Kane <john.kane84@gmail.com>
2010-07-19 03:45:13 +0100

    Now prints files being monitored, at least the first time.

--- src/leiningen/stakeout.clj
+++ src/leiningen/stakeout.clj
@@ -1,9 +1,11 @@
 (ns
-  #^{:doc "Execute a command whenever the given files change."} 
+  #^{:doc "Execute the shell command whenever the given files change."} 
   leiningen.stakeout
-  (:use [stakeout.core :only [stakeout-rec]]))
+  (:use [stakeout.core :only [stakeout-rec get-files]]))
 
 (defn stakeout [project path-glob shell-command]
   (do
-    (println "Watching directory ...")
+    (println "Watching files:")
+    (apply print (map #(str (.toString %)) (get-files path-glob)))
+    (println "")
     (stakeout-rec path-glob shell-command)))
